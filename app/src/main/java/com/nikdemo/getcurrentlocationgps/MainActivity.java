62ecff0fbef75de1f70ecc930c4cbd53702c1d1a
ravipatel <102030@@>
2019-08-13 10:55:01 +0530

    first commit

---
+++ app/src/main/java/com/nikdemo/getcurrentlocationgps/MainActivity.java
@@ -0,0 +1,195 @@
+package com.nikdemo.getcurrentlocationgps;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity  implements LocationListener{
+    private static final String TAG = "MainActivity";
+    LocationManager locationManager;
+    String provider;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    0);
+        }
+        // Getting LocationManager object
+        statusCheck();
+
+        locationManager = (LocationManager) getSystemService(
+                Context.LOCATION_SERVICE);
+
+        // Creating an empty criteria object
+        Criteria criteria = new Criteria();
+
+        // Getting the name of the provider that meets the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+
+        if (provider != null && !provider.equals("")) {
+            if (!provider.contains("gps")) { // if gps is disabled
+                final Intent poke = new Intent();
+                poke.setClassName("com.android.settings",
+                        "com.android.settings.widget.SettingsAppWidgetProvider");
+                poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+                poke.setData(Uri.parse("3"));
+                sendBroadcast(poke);
+            }
+            // Get the location from the given provider
+            Location location = locationManager
+                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 500, 0, this);
+
+            if (location != null)
+                onLocationChanged(location);
+            else
+                location = locationManager.getLastKnownLocation(provider);
+            if (location != null)
+                onLocationChanged(location);
+            else
+
+                Toast.makeText(getBaseContext(), "Location can't be retrieved",
+                        Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(getBaseContext(), "No Provider Found",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void statusCheck() {
+        final LocationManager manager = (LocationManager) getSystemService(
+                Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        }
+    }
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(
+                "Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false).setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog,
+                                        final int id) {
+                        startActivity(new Intent(
+                                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog,
+                                        final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Getting reference to TextView tv_longitude
+        TextView tvLongitude = (TextView) findViewById(R.id.tv_longitude);
+
+        // Getting reference to TextView tv_latitude
+        TextView tvLatitude = (TextView) findViewById(R.id.tv_latitude);
+        TextView editLocation = (TextView) findViewById(R.id.tv_address);
+
+        // Setting Current Longitude
+        tvLongitude.setText("Longitude:" + location.getLongitude());
+
+        // Setting Current Latitude
+        tvLatitude.setText("Latitude:" + location.getLatitude());
+
+        /*----------to get City-Name from coordinates ------------- */
+        String cityName=null;
+        Geocoder gcd = new Geocoder(getBaseContext(),
+                Locale.getDefault());
+        List<Address>  addresses;
+        try {
+            addresses = gcd.getFromLocation(location.getLatitude(), location
+                    .getLongitude(), 1);
+            if (addresses.size() > 0)
+                System.out.println(addresses.get(0).getLocality());
+            cityName=addresses.get(0).getLocality();
+            Log.e(TAG, "onLocationChanged: "+addresses );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String s = location.getLongitude()+"\n"+location.getLatitude() +
+                "\n\nMy Currrent City is: "+cityName;
+        editLocation.setText(s);
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    0);
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+}
